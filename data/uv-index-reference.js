1eb74c07406248fdd8f037253db2e496daa16081
Ubuntu <ubuntu@ip-172-31-88-203.ec2.internal>
2019-09-29 10:20:30 +0000

    Initiated data tabulation to illustrate UV Indexes of cities for different months.

---
+++ data/uv-index-reference.js
@@ -0,0 +1,105 @@
+
+
+
+//Data of illustratio  of UV radiation level changes with season and cities of different latitiudes
+// source of data: https://www.who.int/uv/intersunprogramme/activities/uv_index/en/index3.html
+
+[ 
+    //Country: Argentina, City: Buenos Aires, lat: 34.6037° S, long: 58.3816° W
+    {"country": "Argentine" , "city": "Buenos Aires", "month": "Jan", "uv-index": 9},
+    {"country": "Argentine" , "city": "Buenos Aires", "month": "Feb", "uv-index": 9},
+    {"country": "Argentine" , "city": "Buenos Aires", "month": "Mar", "uv-index": 7},
+    {"country": "Argentine" , "city": "Buenos Aires", "month": "Apr", "uv-index": 4},
+    {"country": "Argentine" , "city": "Buenos Aires", "month": "May", "uv-index": 3},
+    {"country": "Argentine" , "city": "Buenos Aires", "month": "Jun", "uv-index": 2},
+    {"country": "Argentine" , "city": "Buenos Aires", "month": "Jul", "uv-index": 2},
+    {"country": "Argentine" , "city": "Buenos Aires", "month": "Aug", "uv-index": 4},
+    {"country": "Argentine" , "city": "Buenos Aires", "month": "Sep", "uv-index": 5},
+    {"country": "Argentine" , "city": "Buenos Aires", "month": "Oct", "uv-index": 7},
+    {"country": "Argentine" , "city": "Buenos Aires", "month": "Nov", "uv-index": 9},
+    {"country": "Argentine" , "city": "Buenos Aires", "month": "Dec", "uv-index": 10},
+
+    //Country: Australia, City: Darwin lat: 12.4634° S, long: 130.8456° E
+    {"country": "Australia" , "city": "Darwin", "month": "Jan", "uv-index": 12},
+    {"country": "Australia" , "city": "Darwin", "month": "Feb", "uv-index": 13},
+    {"country": "Australia" , "city": "Darwin", "month": "Mar", "uv-index": 12},
+    {"country": "Australia" , "city": "Darwin", "month": "Apr", "uv-index": 10},
+    {"country": "Australia" , "city": "Darwin", "month": "May", "uv-index": 8},
+    {"country": "Australia" , "city": "Darwin", "month": "Jun", "uv-index": 8},
+    {"country": "Australia" , "city": "Darwin", "month": "Jul", "uv-index": 8},
+    {"country": "Australia" , "city": "Darwin", "month": "Aug", "uv-index": 10},
+    {"country": "Australia" , "city": "Darwin", "month": "Sep", "uv-index": 11},
+    {"country": "Australia" , "city": "Darwin", "month": "Oct", "uv-index": 13},
+    {"country": "Australia" , "city": "Darwin", "month": "Nov", "uv-index": 12},
+    {"country": "Australia" , "city": "Darwin", "month": "Dec", "uv-index": 12},
+    
+    //Country: Australia, City: Melbourne lat: 37.8136° S, long: 144.9631° E
+    {"country": "Australia" , "city": "Melbourne", "month": "Jan", "uv-index": 8},
+    {"country": "Australia" , "city": "Melbourne", "month": "Feb", "uv-index": 8},
+    {"country": "Australia" , "city": "Melbourne", "month": "Mar", "uv-index": 6},
+    {"country": "Australia" , "city": "Melbourne", "month": "Apr", "uv-index": 4},
+    {"country": "Australia" , "city": "Melbourne", "month": "May", "uv-index": 2},
+    {"country": "Australia" , "city": "Melbourne", "month": "Jun", "uv-index": 2},
+    {"country": "Australia" , "city": "Melbourne", "month": "Jul", "uv-index": 2},
+    {"country": "Australia" , "city": "Melbourne", "month": "Aug", "uv-index": 3},
+    {"country": "Australia" , "city": "Melbourne", "month": "Sep", "uv-index": 5},
+    {"country": "Australia" , "city": "Melbourne", "month": "Oct", "uv-index": 6},
+    {"country": "Australia" , "city": "Melbourne", "month": "Nov", "uv-index": 8},
+    {"country": "Australia" , "city": "Melbourne", "month": "Dec", "uv-index": 9},  
+    
+    //Country: Australia, City: Sydney lat: 33.8688° S, long: 151.2093° E
+    {"country": "Australia" , "city": "Sydney", "month": "Jan", "uv-index": 9},
+    {"country": "Australia" , "city": "Sydney", "month": "Feb", "uv-index": 9},
+    {"country": "Australia" , "city": "Sydney", "month": "Mar", "uv-index": 7},
+    {"country": "Australia" , "city": "Sydney", "month": "Apr", "uv-index": 5},
+    {"country": "Australia" , "city": "Sydney", "month": "May", "uv-index": 3},
+    {"country": "Australia" , "city": "Sydney", "month": "Jun", "uv-index": 2},
+    {"country": "Australia" , "city": "Sydney", "month": "Jul", "uv-index": 3},
+    {"country": "Australia" , "city": "Sydney", "month": "Aug", "uv-index": 4},
+    {"country": "Australia" , "city": "Sydney", "month": "Sep", "uv-index": 6},
+    {"country": "Australia" , "city": "Sydney", "month": "Oct", "uv-index": 7},
+    {"country": "Australia" , "city": "Sydney", "month": "Nov", "uv-index": 9},
+    {"country": "Australia" , "city": "Sydney", "month": "Dec", "uv-index": 10}, 
+    
+    //Country: Brazil, City: Rio de Janerio lat: 22.9068° S, long: 43.1729° W
+    {"country": "Brazil" , "city": "Rio de Janerio", "month": "Jan", "uv-index": 12},
+    {"country": "Brazil" , "city": "Rio de Janerio", "month": "Feb", "uv-index": 11},
+    {"country": "Brazil" , "city": "Rio de Janerio", "month": "Mar", "uv-index": 9},
+    {"country": "Brazil" , "city": "Rio de Janerio", "month": "Apr", "uv-index": 7},
+    {"country": "Brazil" , "city": "Rio de Janerio", "month": "May", "uv-index": 5},
+    {"country": "Brazil" , "city": "Rio de Janerio", "month": "Jun", "uv-index": 5},
+    {"country": "Brazil" , "city": "Rio de Janerio", "month": "Jul", "uv-index": 5},
+    {"country": "Brazil" , "city": "Rio de Janerio", "month": "Aug", "uv-index": 7},
+    {"country": "Brazil" , "city": "Rio de Janerio", "month": "Sep", "uv-index": 9},
+    {"country": "Brazil" , "city": "Rio de Janerio", "month": "Oct", "uv-index": 10},
+    {"country": "Brazil" , "city": "Rio de Janerio", "month": "Nov", "uv-index": 12},
+    {"country": "Brazil" , "city": "Rio de Janerio", "month": "Dec", "uv-index": 12}, 
+    
+    //Country: Canada, City: Vancouver lat: 49.2827° N, long: 123.1207° W
+    {"country": "Canada" , "city": "Vancouver", "month": "Jan", "uv-index": 1},
+    {"country": "Canada" , "city": "Vancouver", "month": "Feb", "uv-index": 1},
+    {"country": "Canada" , "city": "Vancouver", "month": "Mar", "uv-index": 3},
+    {"country": "Canada" , "city": "Vancouver", "month": "Apr", "uv-index": 4},
+    {"country": "Canada" , "city": "Vancouver", "month": "May", "uv-index": 6},
+    {"country": "Canada" , "city": "Vancouver", "month": "Jun", "uv-index": 7},
+    {"country": "Canada" , "city": "Vancouver", "month": "Jul", "uv-index": 7},
+    {"country": "Canada" , "city": "Vancouver", "month": "Aug", "uv-index": 6},
+    {"country": "Canada" , "city": "Vancouver", "month": "Sep", "uv-index": 4},
+    {"country": "Canada" , "city": "Vancouver", "month": "Oct", "uv-index": 2},
+    {"country": "Canada" , "city": "Vancouver", "month": "Nov", "uv-index": 1},
+    {"country": "Canada" , "city": "Vancouver", "month": "Dec", "uv-index": 1}, 
+
+    //Country: Cuba, City: Havana lat: 23.1136° N, long: 82.3666° W
+    {"country": "Cuba" , "city": "Havana", "month": "Jan", "uv-index": 6},
+    {"country": "Cuba" , "city": "Havana", "month": "Feb", "uv-index": 8},
+    {"country": "Cuba" , "city": "Havana", "month": "Mar", "uv-index": 9},
+    {"country": "Cuba" , "city": "Havana", "month": "Apr", "uv-index": 10},
+    {"country": "Cuba" , "city": "Havana", "month": "May", "uv-index": 10},
+    {"country": "Cuba" , "city": "Havana", "month": "Jun", "uv-index": 11},
+    {"country": "Cuba" , "city": "Havana", "month": "Jul", "uv-index": 12},
+    {"country": "Cuba" , "city": "Havana", "month": "Aug", "uv-index": 11},
+    {"country": "Cuba" , "city": "Havana", "month": "Sep", "uv-index": 10},
+    {"country": "Cuba" , "city": "Havana", "month": "Oct", "uv-index": 8},
+    {"country": "Cuba" , "city": "Havana", "month": "Nov", "uv-index": 6},
+    {"country": "Cuba" , "city": "Havana", "month": "Dec", "uv-index": 5}, 
+]
