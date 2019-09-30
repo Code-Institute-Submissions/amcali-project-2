3d5ad3b868f41491cc9e98db10d0ccd7e4085171
Ubuntu <ubuntu@ip-172-31-88-203.ec2.internal>
2019-09-30 13:52:19 +0000

    Tested data from .csv file, and able to present some data as bar chart on .html file.

--- script.js
+++ script.js
@@ -5,34 +5,49 @@
 /*global crossfilter*/
 /*global d3*/
 /*global dc*/
+/*global uvIndexData*/
 
 queue()
 // file type        // the relative filepath to the .json file
-.defer(d3.json, "data/uv-index-reference.json" ) // load in the json file with uv-index references
+.defer(d3.csv, "data/uv-index-data.csv" ) // load in the json file with uv-index references
+
+.await(makeGraphs);
+
+function makeGraphs(error, uvIndexData){
+ 
+ // create a cross filter
+ let transactionCrossFilter = crossfilter(uvIndexData);
+ 
+ show_uv_readings(transactionCrossFilter);
+ 
+ dc.renderAll();
+ 
+}
 
 
-.await(function(error, transactionData){
-
-      // STEP 1 - create a cross filter
-      let transactionCrossFilter = crossfilter(transactionData);
-        console.log(transactionData);
-       
-    //     //STEP 2 - Define 'month' to be the x axis
-    //   // Creating a dimension based on the 'month' property of each data point
-    //   let month_dim = transactionCrossFilter.dimension(dc.pluck('month'));
-       
-    //     // STEP 3 - Do the grouping
-    //     // "Grouping" --> summarizing each data point
-    //     let uv_month = uv_dim.group().reduceSum(dc.pluck('month'));
-       
-    //     // create a dimension with x axis being the date 
-    //     let uv_dim = transactionCrossFilter.dimension(dc.pluck("month"));
-     
-    //     let min_month = month_num_dim.bottom(1)[0].date;
-    //     let max_month = month_num_dim.top(1)[0].date;
-     
+function show_uv_readings(transactionCrossFilter){
+ 
+   // Define 'month' to be the x axis
+   // Creating a dimension based on the 'month' property of each data point
+   let month_dim = transactionCrossFilter.dimension(dc.pluck('month.num'));
+   
+   // STEP 3 - Do the grouping of dimension by month
+   // "Grouping" --> summarizing each data point
+   let month_group = month_dim.group();
+   
+   dc.barChart("#line-graph")
+    .width(500)
+    .height(400)
+    .margins({top:10, right:50, bottom:30, left:50})
+    .dimension(month_dim)
+    .group(month_group)
+    .transitionDuration(500)
+    .x(d3.scale.ordinal())
+    .xUnits(dc.units.ordinal)
+    .elasticY(true)
+    .xAxisLabel("Month")
+    .yAxis().ticks(13);
         
-    //     // STEP 5 - Draw the barchart
-    //     dc.renderAll();
-    });
+
+}
    
