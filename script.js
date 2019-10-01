7a2c811ce1533bd0c90d3389e726ed75385234a0
Ubuntu <ubuntu@ip-172-31-88-203.ec2.internal>
2019-10-01 04:09:07 +0000

    created bar char with UV index readings for one country.

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ queue()
 function makeGraphs(error, uvIndexData){
  
  // Step 1 - create a cross filter
- let transactionCrossFilter = crossfilter(uvIndexData);  //data from .csv stored as uvIndexData
+ let transactionCrossFilter = crossfilter(uvIndexData);  //data from .json stored as uvIndexData
  console.log(uvIndexData);
  
  show_uv_readings(transactionCrossFilter);
@@ -30,42 +30,81 @@ function show_uv_readings(transactionCrossFilter){
  
    //Step 2 - 
    // Creating a dimension based on the 'month' property of each data point
+   
+   // let dc.pluck('month.num')
    let month_dim = transactionCrossFilter.dimension(dc.pluck('month.num'));
-   let uv_dim = transactionCrossFilter.dimension(dc.pluck('uv.index'))
+   let uv_reading_per_month = month_dim.group().reduce(
+    
+     //Add a fact or data entry
+      function(p, v) {
+       p.count++;
+       p.total += v.uvIndex;
+       p.average = p.total / p.count;
+       return p;
+      },
+      
+     //Remove a fact or data entry
+      function(p, v) {
+      p.count--;
+      if (p.count == 0) {
+          p.total = 0;
+          p.average = 0;
+      } else {
+          p.total -= v.uvIndex;
+          p.average = p.total / p.count;
+      }
+      return p;
+      },     
+     
+     //Initialise the Reducer
+     function () {
+     return { count: 0, total: 0, average: 0};
+     }
+   );
+   
    
    let min_month = month_dim.bottom(1)[0].month.num;
    let max_month = month_dim.top(1)[0].month.num;
    
    // STEP 3 - Do the grouping of dimension by month
    // "Grouping" --> summarizing each data point
-   let uv_index_group = uv_dim.group();
+   let month_group = month_dim.group();
    
-    dc.lineChart("#line-graph")
-    .width(500)
-    .height(400)
-    .margins({top: 10, right: 50, bottom: 30, left: 50})
-    .dimension(month_dim)
-    .group(uv_index_group)
-    .transitionDuration(500)
-    .x(d3.time.scale().domain([min_month,max_month]))
-    .xAxisLabel("Month")
-    .yAxisLabel("UV Index")
-    .yAxis().ticks(4);
+   //STEP 4 - drawing the scales
+   
+    // dc.lineChart("#line-graph")
+    // .width(500)
+    // .height(400)
+    // .margins({top: 10, right: 50, bottom: 30, left: 50})
+    // .dimension(month_dim)
+    // .group(uv_reading_per_month)
+    // .valueAccessor(function (d) {
+    //      return d.value.average;
+    //  })
+    // .transitionDuration(500)
+    // .x(d3.time.scale().domain([min_month,max_month]))
+    // .xAxisLabel("Month")
+    // .yAxisLabel("UV Index")
+    // .yAxis().ticks(4);
    
    //below for bar chart appears, but with wrong presentation of data
-   // dc.barChart("#line-graph")
-   //  .width(800)
-   //  .height(400)
-   //  .margins({top:10, right:50, bottom:30, left:50})
-   //  .dimension(month_dim)
-   //  .group(uv_index_group)
-   //  .transitionDuration(500)
-   //  .x(d3.scale.ordinal())
-   //  .xUnits(dc.units.ordinal)
-   //  .elasticY(true)
-   //  .xAxisLabel("Month")
-   //  .yAxisLabel("UV Index")
-   //  .yAxis().ticks(13);
+   dc.barChart("#line-graph")
+    .width(800)
+    .height(400)
+    .margins({top:10, right:50, bottom:30, left:50})
+    .dimension(month_dim)
+    .group(uv_reading_per_month)
+    .valueAccessor(function (d) {
+         return d.value.average;
+     })
+    .transitionDuration(500)
+    .x(d3.scale.ordinal())
+    .xUnits(dc.units.ordinal)
+    .elasticY(true)
+    .xAxisLabel("Month")
+    .yAxisLabel("UV Index")
+    .yAxis().ticks(13);
+
         
 
 }
