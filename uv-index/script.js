091d2ea4f089d04719bf7cdb5f38026da045bb97
Ubuntu <ubuntu@ip-172-31-88-203.ec2.internal>
2019-10-02 04:14:24 +0000

    relocated index.html & script.js files into 'uv-index' folder.

--- uv-index/script.js
+++ uv-index/script.js
@@ -25,7 +25,7 @@ function makeGraphs(error, uvIndexData){
       d.month = parseDate(d.month);
    });
  
- // show_uv_readings(transactionCrossFilter);
+
  
     //Step 2 - 
    // Creating a dimension based on the 'month' property of each data point
@@ -89,6 +89,64 @@ function makeGraphs(error, uvIndexData){
 
   let compositeChart = dc.compositeChart('#line-graph');
   
+  //require 3 arrays. 1: contain over 20 colours; 2: contain all the cities; 3: an array to push all the colours according to each city.
+  let cityArray = [
+                   "Buenos Aires", 
+                   "Darwin", 
+                   "Melbourne", 
+                   "Sydney", 
+                   "Rio de Janeiro", 
+                   "Vancouver", 
+                   "Havana", 
+                   "Port Stanley", 
+                   "Paris", 
+                   "Berlin", 
+                   "Iraklion", 
+                   "Tokyo",
+                   "Nairobi", 
+                   "Tananarive",
+                   "Maputo",
+                   "Ulan Bator",
+                   "Wellington",
+                   "Panama",
+                   "St Petersbourg",
+                   "Singapore",
+                   "Cape Town",
+                   "Palma de Mallorca",
+                   "Colombo",
+                   "Bangkok",
+                   "Los Angeles",
+                   "New York",
+                   "Hanoi"
+                   ];
+   
+   let colorArray = [
+                  '#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+              		  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+              		  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+              		  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+              		  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+              		  '#66664D', '#991AFF',
+                    ];
+                
+   // function assignColorToCity (cityArray,colorArray){                 
+   //              let count = 1;
+   //              for(var i = 0; i < colorArray.length; i++){
+   //              if(count <= 27){
+   //              count++;
+                
+   //              }
+   //              else{
+   //              console.log(count + cityArray[0] + " choice is " + colorArray[i]);
+   //              count++;
+   //              }
+   //              }
+                
+   // }
+               
+                    
+   console.log(cityArray);                 
+   console.log(colorArray);
   compositeChart
     .width(500)
     .height(400)
@@ -105,12 +163,13 @@ function makeGraphs(error, uvIndexData){
     .legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
     .renderHorizontalGridLines(true)    
     .compose([
-      dc.lineChart(compositeChart)
-          .colors('green')
-          .group(uvOfBuenosAires, 'Buenos Aires'),
-      dc.lineChart(compositeChart)
-          .colors('red')
-          .group(uvofDarwin, 'Darwin'),
+      // dc.lineChart(compositeChart)
+      //     .colors('green')
+      //     .group(uvOfBuenosAires, 'Buenos Aires'),
+      // dc.lineChart(compositeChart)
+      //     .colors('red')
+      //     .group(uvofDarwin, 'Darwin'),
+      
           ])
     .brushOn(false)
     .render();          
