bc9e0c39dbc4f2f98bedf791a3e096ee01fa66e4
amcali <ubuntu@ip-172-31-88-203.ec2.internal>
2019-10-11 09:26:17 +0000

    Enabled map markers to appear according to selected field  in Select Menu.

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ $(function() {
     
     function initPageOne()
     {
-    $("#home").click(function() {
+    $("#about").click(function() {
         $(".page").hide(); // hide all the other pages
         $("#home").show(); // show the home page to user on click
     })
@@ -34,24 +34,25 @@ $(function() {
     
 
 }
-});    
+});  
+
  
 
   //require 3 arrays. 1: contain over 20 colours; 2: contain all the cities; 3: an array to push all the colours according to each city.
   let cityArray = [
-                    {"city": "Buenos Aires", "color": '#FF6633', "longitude": -58.3816, "latitude": -34.6037, "latReading": "35°S"},
-                    {"city": "Darwin", "color": '#FFB399', "longitude": 130.8456, "latitude": -12.4634, "latReading": "13°S"},
-                    {"city": "Melbourne", "color": '#FF33FF', "longitude": 144.9631, "latitude": -37.8136, "latReading": "37°S"},
-                    {"city": "Sydney", "color": '#FFFF99', "longitude": 151.2093, "latitude": -33.8688, "latReading": "34°S"},
-                    {"city": "Rio de Janerio", "color": '#00B3E6', "longitude": -43.1729, "latitude": -22.9068, "latReading": "23°S"},
-                    {"city": "Vancouver", "color": '#E6B333', "longitude": -123.1207, "latitude": 49.2827, "latReading": "49°N"},
-                    {"city": "Havana", "color": '#3366E6', "longitude": -82.3666, "latitude": 23.1136, "latReading": "23°N"},
-                    {"city": "Port Stanley", "color": '#999966', "longitude": -57.8517, "latitude": -51.6977, "latReading": "58°S"},
-                    {"city": "Paris", "color": "#99FF99", "longitude": 2.3522, "latitude": 48.8566, "latReading": "49°N"},
-                    {"city": "Berlin", "color": "#80B300", "longitude": 13.4050, "latitude": 52.5200, "latReading": "52°N"},
-                    {"city": "Heraklion", "color": "#B34D4D", "longitude": 25.1442, "latitude": 35.3387, "latReading": "35°N"},
-                    {"city": "Tokyo", "color": "#809900", "longitude": 139.6503, "latitude": 35.6762, "latReading": "36°N"},
-                    {"city": "Nairobi", "color": "#E6B3B3", "longitude": -36.8219, "latitude": -1.2921, "latReading": "1°S"},
+                    {"country": "Argentine", "city": "Buenos Aires", "color": '#FF6633', "longitude": -58.3816, "latitude": -34.6037, "latReading": "35°S"},
+                    {"country": "Australia", "city": "Darwin", "color": '#FFB399', "longitude": 130.8456, "latitude": -12.4634, "latReading": "13°S"},
+                    {"country": "Australia", "city": "Melbourne", "color": '#FF33FF', "longitude": 144.9631, "latitude": -37.8136, "latReading": "37°S"},
+                    {"country": "Australia", "city": "Sydney", "color": '#FFFF99', "longitude": 151.2093, "latitude": -33.8688, "latReading": "34°S"},
+                    {"country": "Brazil", "city": "Rio de Janerio", "color": '#00B3E6', "longitude": -43.1729, "latitude": -22.9068, "latReading": "23°S"},
+                    {"country": "Canada", "city": "Vancouver", "color": '#E6B333', "longitude": -123.1207, "latitude": 49.2827, "latReading": "49°N"},
+                    {"country": "Cuba", "city": "Havana", "color": '#3366E6', "longitude": -82.3666, "latitude": 23.1136, "latReading": "23°N"},
+                    {"country": "Falkland-Islands", "city": "Port Stanley", "color": '#999966', "longitude": -57.8517, "latitude": -51.6977, "latReading": "58°S"},
+                    {"country": "France", "city": "Paris", "color": "#99FF99", "longitude": 2.3522, "latitude": 48.8566, "latReading": "49°N"},
+                    {"country": "Germany", "city": "Berlin", "color": "#80B300", "longitude": 13.4050, "latitude": 52.5200, "latReading": "52°N"},
+                    {"country": "Greece", "city": "Heraklion", "color": "#B34D4D", "longitude": 25.1442, "latitude": 35.3387, "latReading": "35°N"},
+                    {"country": "Japan", "city": "Tokyo", "color": "#809900", "longitude": 139.6503, "latitude": 35.6762, "latReading": "36°N"},
+                    {"country": "Kenya", "city": "Nairobi", "color": "#E6B3B3", "longitude": -36.8219, "latitude": -1.2921, "latReading": "1°S"},
                     {"city": "Antananarivo", "color": "#6680B3", "longitude": -47.5079, "latitude": -18.8792, "latReading": "19°S"},
                     {"city": "Maputo", "color": "#66991A", "longitude": -32.5732, "latitude": -25.9692, "latReading": "26°S"},
                     {"city": "Ulaanbaatar", "color": "#FF99E6", "longitude": 106.9057, "latitude": 47.8864, "latReading": "48°N"},
@@ -69,67 +70,33 @@ $(function() {
                 ];  //declaring an array to use when drawing the composite chart, and generating information for contents of mapbox popup markers
 
 
-/*Displaying the map */
-mapboxgl.accessToken = 'pk.eyJ1IjoiYW1jYWxpIiwiYSI6ImNrMHl4ZXdzcDA4c3czY3BlcWttc2k3YzkifQ.apM6qnRNX442RufrpJjbyA';
-
-
-// adding map from Mapbox
-let map = new mapboxgl.Map({
-  container: 'map', // #1 HTML container id
-  style: 'mapbox://styles/mapbox/streets-v9', // style URL
-  center: [0, 0], // #2 starting position as [lng, lat]
-  zoom: 0.3,
- });
-
-
-// Save all the markers into an array
-// to be used for later according to drop down selection
-let long_lat_of_cities = [];
-
-for (let p of cityArray) {
-
-    // Place a marker for each place into created array
-    let m = new mapboxgl.Marker()
-    .setLngLat({lng: p.longitude, lat: p.latitude})       //{lng: <lng>, lat: <lat>}
-    .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-    .setHTML(`<h3>${p.city}</h3><p>${p.latReading}</p>'`))
-    .addTo(map);
-
-    // add the created marker to the list of all markers
-    long_lat_of_cities.push(m);
-
-}
-
-map.setRenderWorldCopies(false);
-// map.resize();
-// map.dragPan.disable();
-
-
 /*Creating the Composite Chart*/ 
 queue()
- // file type        // the relative filepath to the .json file
+
+// Data for UV index readings are stored in a local .json file. the following calls the relative filepath to the .json file        
  .defer(d3.json, "data/uv-index-reference.json" ) // load in the json file with uv-index references
  .await(makeGraphs);
 
-function makeGraphs(error, uvIndexData){
 
- // Step 1 - create a cross filter
+/*the following will create a parse method to convert the month from string to a standard month format, 
+and call the functions that create the selector menu and composite chart. */
+function makeGraphs(error, uvIndexData){ 
+
+ // Creating a cross filter
  let transactionCrossFilter = crossfilter(uvIndexData);  //data from .json stored as uvIndexData
- console.log(uvIndexData);
 
 
 let parseDate = d3.time.format("%b").parse;
     uvIndexData.forEach(function(d){
       d.month = parseDate(d.month);
     //   return d.month;
-    console.log(d.month);
 });
 
 
 
 
- show_discipline_selector(transactionCrossFilter);
- show_line_graphs(transactionCrossFilter);
+ show_discipline_selector(transactionCrossFilter);  //calls the selector menu function
+ show_line_graphs(transactionCrossFilter);          //calls the composite chart function
  
 
 
@@ -140,10 +107,14 @@ let parseDate = d3.time.format("%b").parse;
 }    // end of makeGraphs function
 
 
+/*creates the multiple selector menu of cities for use to choose line chart 
+of city choice on composite chart to view the UV index readings from January 
+to December*/
 function show_discipline_selector(transactionCrossFilter){
 
    let city_dim = transactionCrossFilter.dimension(dc.pluck("city"));
    let group = city_dim.group();
+
    dc.selectMenu("#discipline-selector")
     .dimension(city_dim)
     .group(group)
@@ -151,8 +122,6 @@ function show_discipline_selector(transactionCrossFilter){
     .controlsUseVisibility(true)
     .title(kv => kv.key);
     
-    console.log(city_dim);
-    console.log(group);
 }
 
 
@@ -160,8 +129,8 @@ function show_discipline_selector(transactionCrossFilter){
 
 function show_line_graphs(transactionCrossFilter){
 
-    //Step 2 -
-   // Creating a dimension based on the 'month' property of each data point
+   /* Creating a dimension based on the 'month' property of each data point.
+   This will represent the composite chart's x-axis base readings.*/
 
    let month_dim = transactionCrossFilter.dimension(dc.pluck("month"));
    let uv_reading_per_month = month_dim.group().reduce(
@@ -196,12 +165,8 @@ function show_line_graphs(transactionCrossFilter){
 
    let min_month = month_dim.bottom(1)[0].month;
    let max_month = month_dim.top(1)[0].month;
-   
-   console.log(month_dim.bottom(1)[0].month);
-   console.log (month_dim.top(1)[0].month);
 
-   // STEP 3 - Do the grouping of dimension by city
-   // "Grouping" --> summarizing each data point
+   // Dimension grouping (ie. summarising of each data point) is done by city
 
     function uv_by_city(city) {
      return function(d) {
@@ -216,7 +181,8 @@ function show_line_graphs(transactionCrossFilter){
 
 
 
-   // STEP 4 - Do the grouping of dimension by city and color
+   /* Dimension grouping by city and color for purpose to draw different lines
+   on composite chart*/
 
   let compositeChart = dc.compositeChart('#line-graph');
 
@@ -230,12 +196,6 @@ function show_line_graphs(transactionCrossFilter){
          chartsOfLineCharts.push(c);
 
      }
-
-    console.log(chartsOfLineCharts);
-
-
-   //STEP 5 - drawing the graph scales
-
 
 
   compositeChart
@@ -261,64 +221,103 @@ function show_line_graphs(transactionCrossFilter){
     .legend(dc.legend().x(600).y(0).itemHeight(9).gap(5))
     .renderHorizontalGridLines(true)
     .compose(chartsOfLineCharts)     //    .compose(charts)
-    .brushOn(false)
-    .render();
+    .brushOn(false);
+
+    //following to ensure labels on x-axis appear as "Mmm" format    
+    compositeChart.xAxis()
+        .tickFormat(d3.time.format("%b"));
+        
+    compositeChart.render();
 
 
 }  //end of show_line_graphs function
 
-/* to call event on mobile responsive */ //function does not work
-// window.onresize = function(event) {
-//   var newWidth = document.getElementById('box-test').offsetWidth;
-//   compositeChart.width(newWidth)
-//     .transitionDuration(0);
-
-//   dc.renderAll();
-//   compositeChart.transitionDuration(500);
-// };
-
-// function clearmarker() {
-//     for(let m of long_lat_of_cities)
-//         m.remove();
-// }
-
-// function createmarker(place) {
-//     for (let p of cityArray)
-//     {
-//         if (p.city == place)
-//         {
-//             let m = new mapboxgl.Marker()
-//             .setLngLat({lng: p.longitude, lat: p.latitude})       //{lng: <lng>, lat: <lat>}
-//             .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-//             .setHTML(`<h3>${p.city}</h3><p>${p.latReading}</p>`))
-//             .addTo(map);
-        
-//             // add the created marker to the list of all markers
-//             long_lat_of_cities.push(m);
-//         }
-//     }
-// }
 
 
-// $('#discipline-selector').multiSelect({
-//   afterSelect: function(values){
-//     alert("Select value: "+values);
-//   },
-//   afterDeselect: function(values){
-//     alert("Deselect value: "+values);
-//   }
-// });
-
-$( "#dc-select-menu" )
-  .change(function() {
-    console.log("entered");
-    clearmarker();
-    var str = "";
-    $( "#dc-select-menu option:selected" ).each(function() {
-      str += $( this ).text() + " ";
-      createmarker($( this ).text());
+// $( "#dc-select-menu" )
+//   .change(function() {
+//     console.log("entered");
+//     clearmarker();
+//     var str = "";
+//     $( "#dc-select-menu option:selected" ).each(function() {
+//       str += $( this ).text() + " ";
+//       createmarker($( this ).text());
       
+//     });
+//     $( "#test" ).text( str );
+//   })
+//   .trigger( "change" );
+  
+/*Displaying the map */
+mapboxgl.accessToken = 'pk.eyJ1IjoiYW1jYWxpIiwiYSI6ImNrMHl4ZXdzcDA4c3czY3BlcWttc2k3YzkifQ.apM6qnRNX442RufrpJjbyA';
+
+
+// adding map from Mapbox
+let map = new mapboxgl.Map({
+  container: 'map', // #1 HTML container id
+  style: 'mapbox://styles/mapbox/streets-v9', // style URL
+  center: [0, 0], // #2 starting position as [lng, lat]
+  zoom: 0.3,
+ });
+
+
+// Save all the markers into an array
+// to be used for later according to drop down selection
+let long_lat_of_cities = [];
+
+for (let p of cityArray) {
+
+    // Place a marker for each place into created array
+    let m = new mapboxgl.Marker()
+    .setLngLat({lng: p.longitude, lat: p.latitude})       //{lng: <lng>, lat: <lat>}
+    .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+    .setHTML(`'<h2>{$p.country}:</h2><h3>${p.city}</h3><p>${p.latReading}</p>'`))
+    .addTo(map);
+
+    // add the created marker to the list of all markers
+    long_lat_of_cities.push(m);
+
+}
+
+map.setRenderWorldCopies(false);
+// map.resize();
+map.dragPan.disable();
+
+
+/* The following is to enable markers on the map to appear with the graph line according 
+to the city selected from the select menu*/
+
+$(document).on('change', '.dc-select-menu', function () {
+    clearMarkers();
+    $( ".dc-select-menu option:selected" ).each(function() {
+      createmarker($( this ).text());
     });
-    $( "#test" ).text( str );
-  })
-  .trigger( "change" );
+});
+
+function createmarker(place) {
+    let selectall = false;
+    if(place == "Select all")
+        selectall = true;
+    for (let p of cityArray) {
+        if(p.city == place || selectall) {
+            // Place a marker for each place into created array
+            let m = new mapboxgl.Marker()
+            .setLngLat({lng: p.longitude, lat: p.latitude})       //{lng: <lng>, lat: <lat>}
+            .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+            .setHTML(`'<h2>{$p.country}:</h2><h3>${p.city}</h3><p>${p.latReading}</p>'`))
+            .addTo(map);
+        
+            // add the created marker to the list of all markers
+            long_lat_of_cities.push(m);
+            if(!selectall)
+                return;
+        }
+    }
+}
+
+function clearMarkers() {
+    for(let m of long_lat_of_cities) {
+        m.remove();
+    }
+   long_lat_of_cities = [];
+}
