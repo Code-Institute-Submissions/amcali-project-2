a3d741182143f61531309853e95f5696c659c528
amcali <ubuntu@ip-172-31-88-203.ec2.internal>
2019-10-12 07:02:19 +0000

    Organised indentation of script.js file.

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 /*global d3*/
 /*global dc*/
 /* global mapboxgl*/
-/*global axios*/
 /*global $*/
 
 
@@ -27,71 +26,65 @@ $(function() {
 
  
 
-  //require 3 arrays. 1: contain over 20 colours; 2: contain all the cities; 3: an array to push all the colours according to each city.
-  let cityArray = [
-                    {"country": "Argentine", "city": "Buenos Aires", "color": '#FF6633', "longitude": -58.3816, "latitude": -34.6037, "latReading": "35°S"},
-                    {"country": "Australia", "city": "Darwin", "color": '#FFB399', "longitude": 130.8456, "latitude": -12.4634, "latReading": "13°S"},
-                    {"country": "Australia", "city": "Melbourne", "color": '#FF33FF', "longitude": 144.9631, "latitude": -37.8136, "latReading": "37°S"},
-                    {"country": "Australia", "city": "Sydney", "color": '#FFFF99', "longitude": 151.2093, "latitude": -33.8688, "latReading": "34°S"},
-                    {"country": "Brazil", "city": "Rio de Janerio", "color": '#00B3E6', "longitude": -43.1729, "latitude": -22.9068, "latReading": "23°S"},
-                    {"country": "Canada", "city": "Vancouver", "color": '#E6B333', "longitude": -123.1207, "latitude": 49.2827, "latReading": "49°N"},
-                    {"country": "Cuba", "city": "Havana", "color": '#3366E6', "longitude": -82.3666, "latitude": 23.1136, "latReading": "23°N"},
-                    {"country": "Falkland-Islands", "city": "Port Stanley", "color": '#999966', "longitude": -57.8517, "latitude": -51.6977, "latReading": "58°S"},
-                    {"country": "France", "city": "Paris", "color": "#99FF99", "longitude": 2.3522, "latitude": 48.8566, "latReading": "49°N"},
-                    {"country": "Germany", "city": "Berlin", "color": "#80B300", "longitude": 13.4050, "latitude": 52.5200, "latReading": "52°N"},
-                    {"country": "Greece", "city": "Heraklion", "color": "#B34D4D", "longitude": 25.1442, "latitude": 35.3387, "latReading": "35°N"},
-                    {"country": "Japan", "city": "Tokyo", "color": "#809900", "longitude": 139.6503, "latitude": 35.6762, "latReading": "36°N"},
-                    {"country": "Kenya", "city": "Nairobi", "color": "#E6B3B3", "longitude": -36.8219, "latitude": -1.2921, "latReading": "1°S"},
-                    {"country": "Madagascar", "city": "Antananarivo", "color": "#6680B3", "longitude": -47.5079, "latitude": -18.8792, "latReading": "19°S"},
-                    {"country": "Mozambique", "city": "Maputo", "color": "#66991A", "longitude": -32.5732, "latitude": -25.9692, "latReading": "26°S"},
-                    {"country": "Mongolia", "city": "Ulaanbaatar", "color": "#FF99E6", "longitude": 106.9057, "latitude": 47.8864, "latReading": "48°N"},
-                    {"country": "New Zealand", "city": "Wellington", "color": "#CCFF1A", "longitude": 174.7762, "latitude": -41.2865, "latReading": "42°S"},
-                    {"country": "Panama", "city": "Panama", "color": "#FF1A66", "longitude": -80.7821, "latitude": 8.5380, "latReading": "9°N"},
-                    {"country": "Russia", "city": "St Petersburg", "color": "#E6331A", "longitude": 30.3351, "latitude": 59.9343, "latReading": "60°N"},
-                    {"country": "Singapore", "city": "Singapore", "color": "#33FFCC", "longitude": 103.8198, "latitude": 1.3521, "latReading": "1°N"},
-                    {"country": "South Africa", "city": "Cape Town", "color": "#66994D", "longitude": 18.4241, "latitude": -33.9249, "latReading": "34°S"},
-                    {"country": "Spain", "city": "Palma de Mallorca", "color": "#B366CC", "longitude": 2.6502, "latitude": 39.5696, "latReading": "39°N"},
-                    {"country": "Sri Lanka", "city": "Colombo", "color": "#4D8000", "longitude": 79.8612, "latitude": 6.9271, "latReading": "13°N"},
-                    {"country": "Thailand", "city": "Bangkok", "color": "#B33300", "longitude": 100.5018, "latitude": 13.7563, "latReading": "14°N"},
-                    {"country": "USA", "city": "Los Angeles", "color": "#CC80CC", "longitude": -118.2437, "latitude": 34.0522, "latReading": "34°N"},
-                    {"country": "USA", "city": "New York", "color": "#66664D", "longitude": -74.0060, "latitude": 40.7128, "latReading": "41°N"},
-                    {"country": "Vietnam", "city": "Hanoi", "color": "#991AFF", "longitude": 105.8342, "latitude": 21.0278, "latReading": "21°N"}
-                ];  //declaring an array to use when drawing the composite chart, and generating information for contents of mapbox popup markers
+/*Declaring an array to use when drawing the composite chart, 
+and generating information for contents of mapbox popup markers*/
+let cityArray = [
+                {"country": "Argentine", "city": "Buenos Aires", "color": '#FF6633', "longitude": -58.3816, "latitude": -34.6037, "latReading": "35°S"},
+                {"country": "Australia", "city": "Darwin", "color": '#FFB399', "longitude": 130.8456, "latitude": -12.4634, "latReading": "13°S"},
+                {"country": "Australia", "city": "Melbourne", "color": '#FF33FF', "longitude": 144.9631, "latitude": -37.8136, "latReading": "37°S"},
+                {"country": "Australia", "city": "Sydney", "color": '#FFFF99', "longitude": 151.2093, "latitude": -33.8688, "latReading": "34°S"},
+                {"country": "Brazil", "city": "Rio de Janerio", "color": '#00B3E6', "longitude": -43.1729, "latitude": -22.9068, "latReading": "23°S"},
+                {"country": "Canada", "city": "Vancouver", "color": '#E6B333', "longitude": -123.1207, "latitude": 49.2827, "latReading": "49°N"},
+                {"country": "Cuba", "city": "Havana", "color": '#3366E6', "longitude": -82.3666, "latitude": 23.1136, "latReading": "23°N"},
+                {"country": "Falkland-Islands", "city": "Port Stanley", "color": '#999966', "longitude": -57.8517, "latitude": -51.6977, "latReading": "58°S"},
+                {"country": "France", "city": "Paris", "color": "#99FF99", "longitude": 2.3522, "latitude": 48.8566, "latReading": "49°N"},
+                {"country": "Germany", "city": "Berlin", "color": "#80B300", "longitude": 13.4050, "latitude": 52.5200, "latReading": "52°N"},
+                {"country": "Greece", "city": "Heraklion", "color": "#B34D4D", "longitude": 25.1442, "latitude": 35.3387, "latReading": "35°N"},
+                {"country": "Japan", "city": "Tokyo", "color": "#809900", "longitude": 139.6503, "latitude": 35.6762, "latReading": "36°N"},
+                {"country": "Kenya", "city": "Nairobi", "color": "#E6B3B3", "longitude": -36.8219, "latitude": -1.2921, "latReading": "1°S"},
+                {"country": "Madagascar", "city": "Antananarivo", "color": "#6680B3", "longitude": -47.5079, "latitude": -18.8792, "latReading": "19°S"},
+                {"country": "Mozambique", "city": "Maputo", "color": "#66991A", "longitude": -32.5732, "latitude": -25.9692, "latReading": "26°S"},
+                {"country": "Mongolia", "city": "Ulaanbaatar", "color": "#FF99E6", "longitude": 106.9057, "latitude": 47.8864, "latReading": "48°N"},
+                {"country": "New Zealand", "city": "Wellington", "color": "#CCFF1A", "longitude": 174.7762, "latitude": -41.2865, "latReading": "42°S"},
+                {"country": "Panama", "city": "Panama", "color": "#FF1A66", "longitude": -80.7821, "latitude": 8.5380, "latReading": "9°N"},
+                {"country": "Russia", "city": "St Petersburg", "color": "#E6331A", "longitude": 30.3351, "latitude": 59.9343, "latReading": "60°N"},
+                {"country": "Singapore", "city": "Singapore", "color": "#33FFCC", "longitude": 103.8198, "latitude": 1.3521, "latReading": "1°N"},
+                {"country": "South Africa", "city": "Cape Town", "color": "#66994D", "longitude": 18.4241, "latitude": -33.9249, "latReading": "34°S"},
+                {"country": "Spain", "city": "Palma de Mallorca", "color": "#B366CC", "longitude": 2.6502, "latitude": 39.5696, "latReading": "39°N"},
+                {"country": "Sri Lanka", "city": "Colombo", "color": "#4D8000", "longitude": 79.8612, "latitude": 6.9271, "latReading": "13°N"},
+                {"country": "Thailand", "city": "Bangkok", "color": "#B33300", "longitude": 100.5018, "latitude": 13.7563, "latReading": "14°N"},
+                {"country": "USA", "city": "Los Angeles", "color": "#CC80CC", "longitude": -118.2437, "latitude": 34.0522, "latReading": "34°N"},
+                {"country": "USA", "city": "New York", "color": "#66664D", "longitude": -74.0060, "latitude": 40.7128, "latReading": "41°N"},
+                {"country": "Vietnam", "city": "Hanoi", "color": "#991AFF", "longitude": 105.8342, "latitude": 21.0278, "latReading": "21°N"}
+            ];  
 
 
 /*Creating the Composite Chart*/ 
 queue()
 
-// Data for UV index readings are stored in a local .json file. the following calls the relative filepath to the .json file        
- .defer(d3.json, "data/uv-index-reference.json" ) // load in the json file with uv-index references
- .await(makeGraphs);
+    // Data for UV index readings are stored in a local .json file. the following calls the relative filepath to the .json file        
+    .defer(d3.json, "data/uv-index-reference.json" ) // load in the json file with uv-index references
+    .await(makeGraphs);
 
 
 /*the following will create a parse method to convert the month from string to a standard month format, 
 and call the functions that create the selector menu and composite chart. */
 function makeGraphs(error, uvIndexData){ 
 
- // Creating a cross filter
- let transactionCrossFilter = crossfilter(uvIndexData);  //data from .json stored as uvIndexData
+    // Creating a cross filter; data for UVI readings are from in .json file
+    let transactionCrossFilter = crossfilter(uvIndexData);  //data from .json stored as uvIndexData
 
-
-let parseDate = d3.time.format("%b").parse;
+    //To convert readings for each month for compatibility of drawing chart
+    let parseDate = d3.time.format("%b").parse;
     uvIndexData.forEach(function(d){
-      d.month = parseDate(d.month);
-    //   return d.month;
-});
-
-
+        d.month = parseDate(d.month);
+    });
 
 
  show_discipline_selector(transactionCrossFilter);  //calls the selector menu function
  show_line_graphs(transactionCrossFilter);          //calls the composite chart function
- 
 
-
-
- dc.renderAll();
- 
+ dc.renderAll();    //generates all lines on composite chart according to city
 
 }    // end of makeGraphs function
 
@@ -101,15 +94,15 @@ of city choice on composite chart to view the UV index readings from January
 to December*/
 function show_discipline_selector(transactionCrossFilter){
 
-   let city_dim = transactionCrossFilter.dimension(dc.pluck("city"));
-   let group = city_dim.group();
+    let city_dim = transactionCrossFilter.dimension(dc.pluck("city"));
+    let group = city_dim.group();
 
-   dc.selectMenu("#discipline-selector")
-    .dimension(city_dim)
-    .group(group)
-    .multiple(true)
-    .controlsUseVisibility(true)
-    .title(kv => kv.key);
+    dc.selectMenu("#discipline-selector")
+        .dimension(city_dim)
+        .group(group)
+        .multiple(true)
+        .controlsUseVisibility(true)
+        .title(kv => kv.key);       //hides number of readings per city displaying on menu selector
     
 }
 
@@ -118,54 +111,53 @@ function show_discipline_selector(transactionCrossFilter){
 
 function show_line_graphs(transactionCrossFilter){
 
-   /* Creating a dimension based on the 'month' property of each data point.
-   This will represent the composite chart's x-axis base readings.*/
+    /* Creating a dimension based on the 'month' property of each data point.
+    This will represent the composite chart's x-axis base readings.*/
+    
+    let month_dim = transactionCrossFilter.dimension(dc.pluck("month"));
+    let uv_reading_per_month = month_dim.group().reduce(
 
-   let month_dim = transactionCrossFilter.dimension(dc.pluck("month"));
-   let uv_reading_per_month = month_dim.group().reduce(
+         //Add a fact or data entry
+        function(p, v) {
+            p.count++;
+            p.total += v.uvIndex;
+            p.average = p.total / p.count;
+            return p;
+        },
 
-     //Add a fact or data entry
-      function(p, v) {
-       p.count++;
-       p.total += v.uvIndex;
-       p.average = p.total / p.count;
-       return p;
-      },
+        //Remove a fact or data entry
+        function(p, v) {
+        p.count--;
+            if (p.count == 0) {
+              p.total = 0;
+              p.average = 0;
+            } else {
+              p.total -= v.uvIndex;
+              p.average = p.total / p.count;
+            }
+            return p;
+        },
 
-     //Remove a fact or data entry
-      function(p, v) {
-      p.count--;
-      if (p.count == 0) {
-          p.total = 0;
-          p.average = 0;
-      } else {
-          p.total -= v.uvIndex;
-          p.average = p.total / p.count;
-      }
-      return p;
-      },
-
-      //Initialise the Reducer
-      function () {
-      return { count: 0, total: 0, average: 0};
-      }
-   );
+        //Initialise the Reducer
+        function () {
+            return { count: 0, total: 0, average: 0};
+        }
+    );
 
 
    let min_month = month_dim.bottom(1)[0].month;
    let max_month = month_dim.top(1)[0].month;
 
    // Dimension grouping (ie. summarising of each data point) is done by city
-
     function uv_by_city(city) {
-     return function(d) {
-         if (d.city === city) {
-             return +d.uvIndex;
-         }
-         else {
-             return 0;
-         }
-     };
+        return function(d) {
+            if (d.city === city) {
+                return +d.uvIndex;
+            }
+            else {
+                return 0;
+            }
+        };
     }
 
 
@@ -190,29 +182,29 @@ function show_line_graphs(transactionCrossFilter){
   compositeChart
     .width(700)
     .height(400)
-    .margins({top: 10, right: 135, bottom: 75, left: 50})
+    .margins({top: 10, right: 200, bottom: 75, left: 100})
     .renderlet(function (chart) {
-                    chart.selectAll("g.x text")
-                      .attr('dx', '-30')
-                      .attr('dy', '-10')
-                      .attr('transform', "rotate(-90)");
-                })
+        chart.selectAll("g.x text")
+          .attr('dx', '-40')
+          .attr('dy', '-10')
+          .attr('transform', "rotate(-90)");
+    })
     .dimension(month_dim)
     .group(uv_reading_per_month)
     .valueAccessor(function (d) {
-         return d.value.average;
+        return d.value.average;
      })
     .transitionDuration(500)
     .useViewBoxResizing(true)
     .x(d3.time.scale().domain([min_month,max_month]))
     .xAxisLabel("Month")
     .yAxisLabel("UV Index")
-    .legend(dc.legend().x(600).y(0).itemHeight(9).gap(5))
+    .legend(dc.legend().x(525).y(10).itemHeight(9).gap(5))
     .renderHorizontalGridLines(true)
-    .compose(chartsOfLineCharts)     //    .compose(charts)
+    .compose(chartsOfLineCharts)
     .brushOn(false);
 
-    //following to ensure labels on x-axis appear as "Mmm" format    
+    //the following ensures labels on x-axis appear in "Mmm" format    
     compositeChart.xAxis()
         .tickFormat(d3.time.format("%b"));
         
